0e5ea487aa9bbe9a241d95d6bb1766a6442f04e4
visualfc <visualfc@gmail.com>
2024-01-17 10:18:20 +0800

    x

---
+++ gop_autogen.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/visualfc/utiltest/utils"
+)
+//line main.gop:3
+func main() {
+//line main.gop:3:1
+	utils.Test()
+//line main.gop:4:1
+	fmt.Println("world")
+}
---
+++ utils/gop_autogen.go
@@ -0,0 +1,8 @@
+package utils
+
+import "fmt"
+//line utils/utils.gop:7:1
+func Test() {
+//line utils/utils.gop:8:1
+	fmt.Println("test")
+}
