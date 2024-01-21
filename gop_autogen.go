598e1dbc695cc07215f63b24398d5faa518b656b
visualfc <visualfc@gmail.com>
2024-01-21 09:31:23 +0800

    x

--- gop_autogen.go
+++ gop_autogen.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/visualfc/utiltest/utils"
 )
 
-const __gop_autogen__ = true
+func init() {}
+
+const _ = true
+
 //line main.gop:3
 func main() {
 //line main.gop:3:1
