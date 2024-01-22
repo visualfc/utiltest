950c0ed5a33d88898b9ec1beb51405fa6a0d9e52
visualfc <visualfc@gmail.com>
2024-01-22 09:49:42 +0800

    x

--- utils/gop_autogen.go
+++ utils/gop_autogen.go
@@ -2,9 +2,12 @@ package utils
 
 import "fmt"
 
-const __gop_autogen__ = true
+var A string = "hello"
+
+// const __gop_autogen__ = true
+//
 //line utils/utils.gop:7:1
 func Test() {
 //line utils/utils.gop:8:1
-	fmt.Println("test")
+	fmt.Println("test", A)
 }
